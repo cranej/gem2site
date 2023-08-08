f99954a7d1001f04a3bfaa778d00c1ece44c20e7
Crane Jin <crane@cranejin.com>
2023-08-08 20:16:32 +0800

    use template

--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import (
 	"fmt"
 	"git.sr.ht/~justinsantoro/gemtext"
 	"git.sr.ht/~justinsantoro/gemtext/ast"
+	"html/template"
 	"io"
 	"os"
+	"strings"
 )
+
+// "/en/posts/xxxxx.gmi" -> "/en/posts/xxxxx.html"
+func urlReplace(link *ast.Link) {
+	if strings.HasPrefix(link.Url, "/") && strings.HasSuffix(link.Url, ".gmi") {
+		link.Url = strings.TrimSuffix(link.Url, ".gmi") + ".html"
+	}
+}
 
 func line2html(line ast.Line) (string, bool) {
 	switch v := line.(type) {
@@ -15,6 +24,7 @@ func line2html(line ast.Line) (string, bool) {
 	case *ast.Text:
 		return fmt.Sprintf("<p>%s</p>", string(v.Bytes())), false
 	case *ast.Link:
+		urlReplace(v)
 		label := v.Label
 		if label == "" {
 			label = v.Url
@@ -52,30 +62,70 @@ func main() {
 	}
 
 	lines := gemtext.Parse(text)
-	// TODO: use first level 1 heading as title
-	fmt.Println(`<!DOCTYPE html>
+	tmpl, err := template.New("page").Parse(`<!DOCTYPE html>
 	<html>
 	  <head>
 	  	<meta name="generator" content="gem2site">
 		<meta charset="utf-8">
+    	<link href="./site.css" rel="stylesheet"/>
 		<title>
-			cranej&#39;s Webpage
+		{{ .Title }}
 		</title>
 		</head>
-	  <body>`)
+	  <body>
+	  	<main>
+		<article>
+	  	{{ .Content }}
+		</article>
+		</main>
+	  </body>
+	</html>`)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		os.Exit(1)
+	}
 
+	var content strings.Builder
 	inList := false
+	title := ""
 	for _, line := range lines {
+		if title == "" {
+			h1, ok := line.(*ast.Heading)
+			if ok && h1.Level == 1 {
+				title = string(h1.Bytes())
+			}
+		}
+
 		text, isItem := line2html(line)
 		// entering and leaving a list
 		if !inList && isItem {
-			fmt.Println("<ul>")
+			content.WriteString("<ul>\n")
 		} else if inList && !isItem {
-			fmt.Println("</ul>")
+			content.WriteString("</ul>\n")
 		}
-		fmt.Println(text)
+		content.WriteString(text)
+		content.WriteString("\n")
 		inList = isItem
 	}
 
-	fmt.Println("</body></html>")
+	if title == "" {
+		title = "Page"
+	}
+
+	var page strings.Builder
+	data := struct {
+		Title   string
+		Content template.HTML
+	}{
+		Title:   title,
+		Content: template.HTML(content.String()),
+	}
+
+	err = tmpl.Execute(&page, data)
+	if err != nil {
+		fmt.Println("Execute template failed:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(page.String())
 }
